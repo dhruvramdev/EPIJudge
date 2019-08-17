9c948e358237fed6f7655a0de683dac54ce2a54f
Dhruv Ramdev <pythonbrilliant@gmail.com>
2019-08-18 01:58:33 +0530

    Added 5.10, 5.11

--- epi_judge_cpp/rectangle_intersection.cc
+++ epi_judge_cpp/rectangle_intersection.cc
@@ -1,41 +1,81 @@
-#include <tuple>
 #include "test_framework/fmt_print.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
+#include <tuple>
 struct Rect {
-  int x, y, width, height;
+    int x, y, width, height;
 };
 
 Rect IntersectRectangle(const Rect& R1, const Rect& R2) {
-  // TODO - you fill in here.
-  return {0, 0, 0, 0};
+    Rect s, b;
+    Rect inter;
+    if (R1.x < R2.x) {
+        s = R1;
+        b = R2;
+    } else {
+        s = R2;
+        b = R1;
+    }
+    bool xIntersect = false;
+    if (s.x + s.width >= b.x) {
+        xIntersect = true;
+    }
+
+    if (xIntersect) {
+        inter.x = b.x;
+        inter.width = std::min(s.x + s.width, b.x + b.width) - inter.x;
+    }
+
+    if (R1.y < R2.y) {
+        s = R1;
+        b = R2;
+    } else {
+        s = R2;
+        b = R1;
+    }
+    bool yIntersect = false;
+    if (s.y + s.height >= b.y) {
+        yIntersect = true;
+    }
+
+    if (yIntersect) {
+        inter.y = b.y;
+        inter.height = std::min(s.y + s.height, b.y + b.height) - inter.y;
+    }
+
+    if (xIntersect && yIntersect) {
+        return inter;
+    } else {
+        return {0, 0, -1, -1};
+    }
 }
+
 bool operator==(const Rect& r1, const Rect& r2) {
-  return std::tie(r1.x, r1.y, r1.width, r1.height) ==
-         std::tie(r2.x, r2.y, r2.width, r2.height);
+    return std::tie(r1.x, r1.y, r1.width, r1.height) ==
+           std::tie(r2.x, r2.y, r2.width, r2.height);
 }
 
 template <>
 struct SerializationTraits<Rect> : UserSerTraits<Rect, int, int, int, int> {
-  static std::vector<std::string> GetMetricNames(const std::string& arg_name) {
-    return {FmtStr("area({})", arg_name), FmtStr("perimeter({})", arg_name),
-            FmtStr("max(w, h)({})", arg_name)};
-  }
+    static std::vector<std::string> GetMetricNames(const std::string& arg_name) {
+        return {FmtStr("area({})", arg_name), FmtStr("perimeter({})", arg_name),
+                FmtStr("max(w, h)({})", arg_name)};
+    }
 
-  static std::vector<int> GetMetrics(const Rect& x) {
-    return {x.height * x.width, 2 * (x.height + x.width),
-            std::max(x.height, x.width)};
-  }
+    static std::vector<int> GetMetrics(const Rect& x) {
+        return {x.height * x.width, 2 * (x.height + x.width),
+                std::max(x.height, x.width)};
+    }
 };
 
 std::ostream& operator<<(std::ostream& out, const Rect& r) {
-  return PrintTo(out, std::make_tuple(r.x, r.y, r.width, r.height));
+    return PrintTo(out, std::make_tuple(r.x, r.y, r.width, r.height));
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"R1", "R2"};
-  return GenericTestMain(args, "rectangle_intersection.cc",
-                         "rectangle_intersection.tsv", &IntersectRectangle,
-                         DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"R1", "R2"};
+    return GenericTestMain(args, "rectangle_intersection.cc",
+                           "rectangle_intersection.tsv", &IntersectRectangle,
+                           DefaultComparator{}, param_names);
 }
--- epi_judge_cpp/uniform_random_number.cc
+++ epi_judge_cpp/uniform_random_number.cc
@@ -1,50 +1,69 @@
-#include <functional>
-#include <random>
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/random_sequence_checker.h"
 #include "test_framework/timed_executor.h"
+#include <functional>
+#include <random>
+#include <vector>
 using std::bind;
 using std::default_random_engine;
 using std::random_device;
 using std::uniform_int_distribution;
 using std::vector;
 int ZeroOneRandom() {
-  default_random_engine gen((random_device())());
-  uniform_int_distribution<int> dis(0, 1);
-  return dis(gen);
+    default_random_engine gen((random_device())());
+    uniform_int_distribution<int> dis(0, 1);
+    return dis(gen);
 }
 
 int UniformRandom(int lower_bound, int upper_bound) {
-  // TODO - you fill in here.
-  return 0;
+    int n = upper_bound - lower_bound;
+    int outcomes = 1;
+    while (outcomes <= n) {
+        outcomes <<= 1;
+    }
+    int tosses = log2(outcomes);
+    bool found = false;
+    int number;
+    while (!found) {
+        number = 0;
+        for (int i = 0; i < tosses; i++) {
+            number <<= 1;
+            number = number | ZeroOneRandom();
+        }
+        if (number <= n) {
+            found = true;
+        }
+    }
+    return lower_bound + number;
 }
+
+
 bool UniformRandomRunner(TimedExecutor& executor, int lower_bound,
                          int upper_bound) {
-  vector<int> result;
-  executor.Run([&] {
-    std::generate_n(back_inserter(result), 100000,
-                    std::bind(UniformRandom, lower_bound, upper_bound));
-  });
+    vector<int> result;
+    executor.Run([&] {
+        std::generate_n(back_inserter(result), 100000,
+                        std::bind(UniformRandom, lower_bound, upper_bound));
+    });
 
-  vector<int> sequence;
-  std::transform(begin(result), end(result), back_inserter(sequence),
-                 [lower_bound](int result) { return result - lower_bound; });
-  return CheckSequenceIsUniformlyRandom(result, upper_bound - lower_bound + 1,
-                                        0.01);
+    vector<int> sequence;
+    std::transform(begin(result), end(result), back_inserter(sequence),
+                   [lower_bound](int result) { return result - lower_bound; });
+    return CheckSequenceIsUniformlyRandom(result, upper_bound - lower_bound + 1,
+                                          0.01);
 }
 
 void UniformRandomWrapper(TimedExecutor& executor, int lower_bound,
                           int upper_bound) {
-  RunFuncWithRetries(
-      bind(UniformRandomRunner, std::ref(executor), lower_bound, upper_bound));
+    RunFuncWithRetries(
+        bind(UniformRandomRunner, std::ref(executor), lower_bound, upper_bound));
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "lower_bound",
-                                       "upper_bound"};
-  return GenericTestMain(args, "uniform_random_number.cc",
-                         "uniform_random_number.tsv", &UniformRandomWrapper,
-                         DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"executor", "lower_bound",
+                                         "upper_bound"};
+    return GenericTestMain(args, "uniform_random_number.cc",
+                           "uniform_random_number.tsv", &UniformRandomWrapper,
+                           DefaultComparator{}, param_names);
 }
