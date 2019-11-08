bc475e6602bb1ea118c5e645498766ee104a893c
Dhruv Ramdev <dhruvramdev@outlook.com>
2019-11-09 02:28:49 +0530

    Added 9.1 and 9.7

--- epi_judge_cpp/stack_with_max.cc
+++ epi_judge_cpp/stack_with_max.cc
@@ -1,31 +1,68 @@
-#include <stdexcept>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_failure.h"
+#include <stack>
+#include <stdexcept>
 using std::length_error;
-
+using std::stack;
 class Stack {
- public:
-  bool Empty() const {
-    // TODO - you fill in here.
-    return true;
-  }
-  int Max() const {
-    // TODO - you fill in here.
-    return 0;
-  }
-  int Pop() {
-    // TODO - you fill in here.
-    return 0;
-  }
-  void Push(int x) {
-    // TODO - you fill in here.
-    return;
-  }
+  private:
+    stack<int> s;
+    stack<int> q;
+    // Soln acc to book
+    // Use Another Stack to keep track changes of max with their consective counts in push since they can be elements that are maximum. 
+    // element at top of this stack will be max.
+
+    int max;
+
+    void findMax() {
+        int temp;
+        max = s.top();
+        while (!s.empty()) {
+            temp = s.top();
+
+            if (max < temp) {
+                max = temp;
+            }
+            q.push(temp);
+            s.pop();
+        }
+        while (!q.empty()) {
+            s.push(q.top());
+            q.pop();
+        }
+    }
+
+  public:
+    bool Empty() const {
+        return s.empty();
+    }
+    int Max() const {
+        return max;
+    }
+    int Pop() {
+        int temp = s.top();
+        s.pop();
+        if (temp == max && !s.empty()) {
+            findMax();
+        }
+        return temp;
+    }
+    void Push(int x) {
+        if (s.empty()) {
+            max = x;
+        } else {
+            if (x > max) {
+                max = x;
+            }
+        }
+        s.push(x);
+        return;
+    }
 };
 struct StackOp {
-  std::string op;
-  int argument;
+    std::string op;
+    int argument;
 };
 
 template <>
@@ -33,43 +70,43 @@ struct SerializationTraits<StackOp> : UserSerTraits<StackOp, std::string, int> {
 };
 
 void StackTester(const std::vector<StackOp>& ops) {
-  try {
-    Stack s;
-    for (auto& x : ops) {
-      if (x.op == "Stack") {
-        continue;
-      } else if (x.op == "push") {
-        s.Push(x.argument);
-      } else if (x.op == "pop") {
-        int result = s.Pop();
-        if (result != x.argument) {
-          throw TestFailure("Pop: expected " + std::to_string(x.argument) +
-                            ", got " + std::to_string(result));
+    try {
+        Stack s;
+        for (auto& x : ops) {
+            if (x.op == "Stack") {
+                continue;
+            } else if (x.op == "push") {
+                s.Push(x.argument);
+            } else if (x.op == "pop") {
+                int result = s.Pop();
+                if (result != x.argument) {
+                    throw TestFailure("Pop: expected " + std::to_string(x.argument) +
+                                      ", got " + std::to_string(result));
+                }
+            } else if (x.op == "max") {
+                int result = s.Max();
+                if (result != x.argument) {
+                    throw TestFailure("Max: expected " + std::to_string(x.argument) +
+                                      ", got " + std::to_string(result));
+                }
+            } else if (x.op == "empty") {
+                int result = s.Empty();
+                if (result != x.argument) {
+                    throw TestFailure("Empty: expected " + std::to_string(x.argument) +
+                                      ", got " + std::to_string(result));
+                }
+            } else {
+                throw std::runtime_error("Unsupported stack operation: " + x.op);
+            }
         }
-      } else if (x.op == "max") {
-        int result = s.Max();
-        if (result != x.argument) {
-          throw TestFailure("Max: expected " + std::to_string(x.argument) +
-                            ", got " + std::to_string(result));
-        }
-      } else if (x.op == "empty") {
-        int result = s.Empty();
-        if (result != x.argument) {
-          throw TestFailure("Empty: expected " + std::to_string(x.argument) +
-                            ", got " + std::to_string(result));
-        }
-      } else {
-        throw std::runtime_error("Unsupported stack operation: " + x.op);
-      }
+    } catch (length_error&) {
+        throw TestFailure("Unexpected length_error exception");
     }
-  } catch (length_error&) {
-    throw TestFailure("Unexpected length_error exception");
-  }
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"ops"};
-  return GenericTestMain(args, "stack_with_max.cc", "stack_with_max.tsv",
-                         &StackTester, DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"ops"};
+    return GenericTestMain(args, "stack_with_max.cc", "stack_with_max.tsv",
+                           &StackTester, DefaultComparator{}, param_names);
 }
--- epi_judge_cpp/tree_level_order.cc
+++ epi_judge_cpp/tree_level_order.cc
@@ -1,20 +1,50 @@
-#include <memory>
-#include <vector>
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
-using std::unique_ptr;
-using std::vector;
+#include <memory>
+#include <queue>
+#include <vector>
+using namespace std;
 
 vector<vector<int>> BinaryTreeDepthOrder(
     const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return {};
+    // TODO - you fill in here.
+    BinaryTreeNode<int>* root = tree.get();
+    vector<vector<int>> result = {};
+
+    if (root == NULL) {
+        return result;
+    }
+
+    int level = 0;
+    queue<pair<int, BinaryTreeNode<int>*>> q;
+    q.push(make_pair(0, root));
+
+    pair<int, BinaryTreeNode<int>*> t;
+    while (!q.empty()) {
+        t = q.front();
+        q.pop();
+        while (result.size() <= t.first) {
+            vector<int> p;
+            result.push_back(p);
+        }
+
+        result[t.first].push_back(t.second->data);
+
+        if (t.second->left != NULL) {
+            q.push(make_pair(t.first + 1, t.second->left.get()));
+        }
+        if (t.second->right != NULL) {
+            q.push(make_pair(t.first + 1, t.second->right.get()));
+        }
+    }
+
+    return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree"};
-  return GenericTestMain(args, "tree_level_order.cc", "tree_level_order.tsv",
-                         &BinaryTreeDepthOrder, DefaultComparator{},
-                         param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"tree"};
+    return GenericTestMain(args, "tree_level_order.cc", "tree_level_order.tsv",
+                           &BinaryTreeDepthOrder, DefaultComparator{},
+                           param_names);
 }
