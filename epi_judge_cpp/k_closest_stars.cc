366d121e4a96c26cd852d0c6e6342788ff5fcf5c
Dhruv Ramdev <dhruvramdev@outlook.com>
2019-11-16 17:11:12 +0530

    Added 11.1 and 11.4

--- epi_judge_cpp/k_closest_stars.cc
+++ epi_judge_cpp/k_closest_stars.cc
@@ -1,57 +1,77 @@
-#include <algorithm>
-#include <cmath>
-#include <vector>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_utils.h"
-using std::vector;
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <vector>
+using namespace std;
 
 struct Star {
-  bool operator<(const Star& that) const {
-    return Distance() < that.Distance();
-  }
+    bool operator<(const Star& that) const {
+        return Distance() < that.Distance();
+    }
 
-  double Distance() const { return sqrt(x * x + y * y + z * z); }
+    double Distance() const { return sqrt(x * x + y * y + z * z); }
 
-  double x, y, z;
+    double x, y, z;
 };
 
 vector<Star> FindClosestKStars(vector<Star>::const_iterator stars_begin,
                                const vector<Star>::const_iterator& stars_end,
                                int k) {
-  // TODO - you fill in here.
-  return {};
+    priority_queue<Star> pq;
+
+    double temp;
+    for (; stars_begin != stars_end; stars_begin++) {
+        temp = stars_begin->Distance();
+        if (pq.size() < k) {
+            pq.push(*stars_begin);
+        } else if (temp < pq.top().Distance()) {
+            pq.pop();
+            pq.push(*stars_begin);
+        }
+    }
+
+    vector<Star> result;
+    while (!pq.empty()) {
+        result.push_back(pq.top());
+        pq.pop();
+    }
+    // reverse(result.begin(), result.end());
+    // return result;
+    return {result.rbegin(), result.rend()};
 }
 template <>
 struct SerializationTraits<Star> : UserSerTraits<Star, double, double, double> {
 };
 
 std::ostream& operator<<(std::ostream& out, const Star& s) {
-  return out << s.Distance();
+    return out << s.Distance();
 }
 
 bool Comp(const vector<double>& expected, vector<Star> output) {
-  if (output.size() != expected.size()) {
-    return false;
-  }
-
-  std::sort(begin(output), end(output));
-
-  for (int i = 0; i < output.size(); ++i) {
-    if (!DefaultComparator()(output[i].Distance(), expected[i])) {
-      return false;
+    if (output.size() != expected.size()) {
+        return false;
     }
-  }
-  return true;
+
+    std::sort(begin(output), end(output));
+
+    for (int i = 0; i < output.size(); ++i) {
+        if (!DefaultComparator()(output[i].Distance(), expected[i])) {
+            return false;
+        }
+    }
+    return true;
 }
 
 vector<Star> FindClosestKStarsWrapper(const vector<Star>& stars, int k) {
-  return FindClosestKStars(cbegin(stars), cend(stars), k);
+    return FindClosestKStars(cbegin(stars), cend(stars), k);
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"stars", "k"};
-  return GenericTestMain(args, "k_closest_stars.cc", "k_closest_stars.tsv",
-                         &FindClosestKStarsWrapper, &Comp, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"stars", "k"};
+    return GenericTestMain(args, "k_closest_stars.cc", "k_closest_stars.tsv",
+                           &FindClosestKStarsWrapper, &Comp, param_names);
 }
--- epi_judge_cpp/sorted_arrays_merge.cc
+++ epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,15 +1,36 @@
-#include <vector>
 #include "test_framework/generic_test.h"
-using std::vector;
+#include <queue>
+#include <vector>
+using namespace std;
+
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> p;
+
+    int no_arrays = sorted_arrays.size();
+    int indexes[no_arrays];
+    for (int i = 0 ; i < no_arrays ; i++) {
+        indexes[i] = 1;
+        p.push(make_pair(sorted_arrays[i][0], i));
+    }
+    vector<int> result;
+    pair<int, int> temp;
+    while (!p.empty()) {
+        temp = p.top();
+        p.pop();
+        result.push_back(temp.first);
+        if (indexes[temp.second] < sorted_arrays[temp.second].size()) {
+            p.push(make_pair(sorted_arrays[temp.second][indexes[temp.second]], temp.second));
+            indexes[temp.second]++;
+        }
+    }
+
+    return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"sorted_arrays"};
-  return GenericTestMain(args, "sorted_arrays_merge.cc",
-                         "sorted_arrays_merge.tsv", &MergeSortedArrays,
-                         DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"sorted_arrays"};
+    return GenericTestMain(args, "sorted_arrays_merge.cc",
+                           "sorted_arrays_merge.tsv", &MergeSortedArrays,
+                           DefaultComparator{}, param_names);
 }
