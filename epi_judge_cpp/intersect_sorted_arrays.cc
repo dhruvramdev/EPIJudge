217a06f49356cd888dc5d41f1cbba07dc257c2f6
Dhruv Ramdev <dhruvramdev@outlook.com>
2019-12-21 23:52:58 +0530

    Added 14.1 and 14.2

--- epi_judge_cpp/intersect_sorted_arrays.cc
+++ epi_judge_cpp/intersect_sorted_arrays.cc
@@ -1,17 +1,42 @@
-#include <vector>
 #include "test_framework/generic_test.h"
-using std::vector;
+#include <vector>
+using namespace std;
 
-vector<int> IntersectTwoSortedArrays(const vector<int>& A,
-                                     const vector<int>& B) {
-  // TODO - you fill in here.
-  return {};
+vector<int> IntersectTwoSortedArrays(const vector<int>& A, const vector<int>& B) {
+    if (A.size() == 0 || B.size() == 0) {
+        return {};
+    }
+    
+    int last = A[0] + 1;
+    int index = 0;
+    int max = B.size();
+    
+    vector<int> result;
+
+    for (auto elem : A) {
+        if (elem != last) {
+            last = elem;
+            while(index < max){
+                if(B[index] < elem){
+                    index++;
+                } else if(B[index] == elem){
+                    index++;
+                    result.push_back(elem);
+                    break;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "B"};
-  return GenericTestMain(
-      args, "intersect_sorted_arrays.cc", "intersect_sorted_arrays.tsv",
-      &IntersectTwoSortedArrays, DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A", "B"};
+    return GenericTestMain(
+        args, "intersect_sorted_arrays.cc", "intersect_sorted_arrays.tsv",
+        &IntersectTwoSortedArrays, DefaultComparator{}, param_names);
 }
--- epi_judge_cpp/two_sorted_arrays_merge.cc
+++ epi_judge_cpp/two_sorted_arrays_merge.cc
@@ -1,21 +1,41 @@
-#include <vector>
 #include "test_framework/generic_test.h"
-using std::vector;
+#include <vector>
+using namespace std;
 
 void MergeTwoSortedArrays(vector<int>& A, int m, const vector<int>& B, int n) {
-  // TODO - you fill in here.
-  return;
+    int index = m + n - 1;
+    n -= 1;
+    m -= 1;
+
+    while (n >= 0 && m >= 0) {
+        if (A[m] > B[n]) {
+            A[index] = A[m];
+            m--;
+        } else {
+            A[index] = B[n];
+            n--;
+        }
+        index--;
+    }
+
+    while (n >= 0) {
+        A[index] = B[n];
+        n--;
+        index--;
+    }
+
+    return;
 }
 vector<int> MergeTwoSortedArraysWrapper(vector<int> A, int m,
                                         const vector<int>& B, int n) {
-  MergeTwoSortedArrays(A, m, B, n);
-  return A;
+    MergeTwoSortedArrays(A, m, B, n);
+    return A;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "m", "B", "n"};
-  return GenericTestMain(
-      args, "two_sorted_arrays_merge.cc", "two_sorted_arrays_merge.tsv",
-      &MergeTwoSortedArraysWrapper, DefaultComparator{}, param_names);
+    std::vector<std::string> args{argv + 1, argv + argc};
+    std::vector<std::string> param_names{"A", "m", "B", "n"};
+    return GenericTestMain(
+        args, "two_sorted_arrays_merge.cc", "two_sorted_arrays_merge.tsv",
+        &MergeTwoSortedArraysWrapper, DefaultComparator{}, param_names);
 }
